22e847f4943b662bd3673ee918b6b0801f5713e7
Will Charczuk <will@clotheshor.se>
2013-12-28 21:54:43 -0500

    actual positioning!!1

--- extension.js
+++ extension.js
@@ -22,40 +22,11 @@ var Hoverable = (function(){
 
 	hvr.Mouse = {};
 
-
 	hvr.container 	= $('<div class="hoverable-container"></div>');
 	hvr.media 		= $('<div class="hoverable-media"></div>');
 	hvr.loader 		= $('<div class="hoverable-loader"></div>');
 	hvr.img 		= $('<div class="hoverable-image"/>');
 
-	$(hvr.media).append(hvr.loader);
-	$(hvr.media).append(hvr.img);
-
-	$(window).on('mousemove', 'body', function(e) {
-		e = e || window.event;
-	    var cursor = {
-	      x: 0,
-	      y: 0
-	    };
-	    if (e.pageX || e.pageY) {
-	      cursor.x = e.pageX;
-	      cursor.y = e.pageY;
-	    } else {
-	      cursor.x = e.clientX +
-	        (document.documentElement.scrollLeft ||
-	        document.body.scrollLeft) -
-	        document.documentElement.clientLeft;
-	      cursor.y = e.clientY +
-	        (document.documentElement.scrollTop ||
-	        document.body.scrollTop) -
-	        document.documentElement.clientTop;
-	    }
-	    hvr.Mouse = {
-	      x: cursor.x,
-	      y: cursor.y
-	    };
-	});
-	//END EVENT HOOKS
 
 	hvr.hide = function() {
 		cancelAlbumTimer();
@@ -66,6 +37,15 @@ var Hoverable = (function(){
 
 	hvr.show = function(media) {
 		
+		var onImgLoad = function(img) {
+			$(hvr.loader).hide();
+			$(hvr.img).data("height", img.height);
+			$(hvr.img).data("width", img.width);
+			$(hvr.img).append(img);
+			$(hvr.img).show();
+			hvr.positionContainer();
+		}
+
 		$(hvr.img).hide();
 		$(hvr.loader).show();
 
@@ -76,21 +56,11 @@ var Hoverable = (function(){
 			$(image).attr('src', media.src);
 
 			$(image).on("load", function() {
-				$(hvr.loader).hide();
-				$(hvr.img).data("height", image.height);
-				$(hvr.img).data("width", image.width);
-				$(hvr.img).append(image);
-				$(hvr.img).show();
-				hvr.positionContainer();
+				onImgLoad(image);
 			});
 
 			if(hvr.lastUrl == media.src) {
-				$(hvr.loader).hide();
-				$(hvr.img).data("height", image.height);
-				$(hvr.img).data("width", image.width);
-				$(hvr.img).append(image);
-				$(hvr.img).show();
-				hvr.positionContainer();
+				onImgLoad(image);
 			}
 		} else if (media.type == mediaTypes.album) {
 			$(hvr.img).empty();
@@ -101,15 +71,7 @@ var Hoverable = (function(){
 				if(i == 0) {
 					$(newImg).show();
 					$(newImg).on("load", function() {
-						
-						$(hvr.img).data("height", image.height);
-						$(hvr.img).data("width", image.width);
-	
-						$(hvr.img).append(newImg);
-						$(hvr.loader).hide();
-						$(hvr.img).show();
-						hvr.positionContainer();
-
+						onImgLoad(newImg);
 						hvr.albumTimer = setTimeout(advanceAlbumImage, 1500);
 					});
 				} else {
@@ -129,7 +91,7 @@ var Hoverable = (function(){
 				$(hvr.img).find("img").hide();
 				$($(hvr.img).find("img")[current_index]).show();
 				$(hvr.img).data("album-index", current_index);
-
+				hvr.positionContainer();
 				hvr.albumTimer = setTimeout(advanceAlbumImage, 1500);
 			}
 
@@ -151,24 +113,23 @@ var Hoverable = (function(){
 		var image_height = $(hvr.loader).height();
 		var image_width = $(hvr.loader).width();
 
-		if($(hvr.img).css('display') !== 'none') {
+		if($(hvr.img).css('display') !== 'none') { //if the image is showing
 			image_height = $(hvr.img).data("height");
 			image_width = $(hvr.img).data("width");
 		}
 
 		var landscape = image_height < image_width;
 
-		var image_height_half = image_height >> 1;
-		var image_width_half = image_width >> 1;
+		var scrollTop = $(window).scrollTop();
 
 		var mouse_x = hvr.Mouse.x;
-		var mouse_y = hvr.Mouse.y;
+		var mouse_y = hvr.Mouse.y - scrollTop;
 
-		var window_width_buffer_left = window_width - mouse_x;
-		var window_width_buffer_right = window_width - (window_width - mouse_x);
+		var window_width_buffer_left = window_width - (window_width - mouse_x);
+		var window_width_buffer_right = window_width - mouse_x;
 
-		var window_height_buffer_up = window_height - mouse_y;
-		var window_height_buffer_down = window_height - (window_height - mouse_y);
+		var window_height_buffer_up = window_height - (window_height - mouse_y);
+		var window_height_buffer_down = window_height - mouse_y;
 
 		var window_x_mid = window_width >> 1; //div by 2
 		var window_y_mid = window_height >> 1; //div by 2
@@ -181,45 +142,64 @@ var Hoverable = (function(){
 		var right = null;
 		var bottom = null;
 
+		var aspect_ratio = image_width / image_height;
+
+		var orient_up = mouse_y > window_y_mid; 
+		var orient_left = mouse_x > window_x_mid;
+
 		var height_buffer = window_height_buffer_down;
-		if(mouse_y > window_y_mid) {
+		if(orient_up) {
 			height_buffer = window_height_buffer_up;
 		}
 
 		var width_buffer = window_width_buffer_right;
-		if(mouse_x > window_x_mid) {
+		if(orient_left) {
 			width_buffer = window_width_buffer_left
 		}
 
-		var aspect_ratio = image_width / image_height;
+		var final_width = image_width;
+		var final_height = image_height;
 
-		if(landscape) {
-			if(image_width > width_buffer) {
-				if(mouse_x > window_x_mid) {
-					left = mouse_x - (width_buffer + padding);
-				} else {
-					left = mouse_x + width_buffer + padding;
-				}
-				top = mouse_y - image_height_half;
-			} else {
-				if(mouse_x > window_x_mid) {
-					left = mouse_x - (image_width + padding);
-					
-				} else {
-					left = mouse_x + image_width + padding;
-				}
-			}
-			max_width = width_buffer;
+		if (landscape) { //means we care about fitting the width more than the height. 
+			if(image_width > width_buffer) final_width = width_buffer - padding;
 
+			final_height = final_width / aspect_ratio;
+		} else { //portrait mode bitches.
+			if (image_height > window_height) final_height = window_height;
+			final_width = final_height * aspect_ratio;
 
-		} else { //portrait
-			if(image_height > height_buffer) {
-				max_height = window_height;
-				top = 0;
-			} else {
-				top = mouse_y - image_height_half;
+			if(final_width > width_buffer) {
+				final_width = width_buffer - padding;
+				final_height = final_width / aspect_ratio;
 			}
 		}
+
+		var final_height_half = final_height >> 1; //we only really care about this one.
+		var final_width_half = final_width >> 1;
+
+		if(landscape) {
+			if(orient_left) left = mouse_x - (final_width + padding);
+			else 			left = mouse_x + padding;
+			
+			if(final_height >= window_height) 	top = 0;
+			else 								top =  mouse_y - final_height_half;
+
+		} else { //portrait
+			if(image_height > window_height) 	top = 0;
+			else 								top = mouse_y - final_height_half;
+
+			if(orient_left) left = mouse_x - (final_width + padding);
+			else 			left = mouse_x + padding;
+		}
+
+		if(top < 0) top = 0;
+		if(left < 0) left = 0;
+
+		max_width = final_width;
+		max_height = final_height;
+
+
+		//from here on out this assumes we've squared away all key variables.
 
 		//max-widths ...
 		$(hvr.container).css('max-width', max_width + "px");
@@ -507,10 +487,38 @@ var Hoverable = (function(){
 		},
 	};
 
+	$(hvr.media).append(hvr.loader);
+	$(hvr.media).append(hvr.img);
+
 	$(hvr.container)
       .append(hvr.media)
       .mouseleave(onMouseOut)
       .mouseover(cancelHideTimer);
+
+	$(window).on('mousemove', 'body', function(e) {
+		e = e || window.event;
+	    var cursor = {
+	      x: 0,
+	      y: 0
+	    };
+	    if (e.pageX || e.pageY) {
+	      cursor.x = e.pageX;
+	      cursor.y = e.pageY;
+	    } else {
+	      cursor.x = e.clientX +
+	        (document.documentElement.scrollLeft ||
+	        document.body.scrollLeft) -
+	        document.documentElement.clientLeft;
+	      cursor.y = e.clientY +
+	        (document.documentElement.scrollTop ||
+	        document.body.scrollTop) -
+	        document.documentElement.clientTop;
+	    }
+	    hvr.Mouse = {
+	      x: cursor.x,
+	      y: cursor.y
+	    };
+	});
 
 	$(document.body).append(hvr.container);
 
